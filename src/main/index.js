842deb382d61c056018d38e034078990d19e6f07
Yauhen Shulitski <jsnjack@gmail.com>
2018-06-02 22:58:05 +0200

    Add icons

--- src/main/index.js
+++ src/main/index.js
@@ -18,8 +18,8 @@ function createWindow() {
    * Initial window options
    */
     mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 600,
+        width: 1200,
+        height: 800,
         autoHideMenuBar: true,
     });
 
