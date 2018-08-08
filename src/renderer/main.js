61daa17c824a0669d734eaeb7a908c7acd4a9a1a
Yauhen Shulitski <jsnjack@gmail.com>
2018-08-08 23:39:13 +0300

    :p: Allow opening dev console in production

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
 
+import {remote} from "electron";
 import App from "./App";
 
 if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
@@ -12,3 +13,12 @@ new Vue({
     components: {App},
     template: "<App/>",
 }).$mount("#app");
+
+
+remote.globalShortcut.register("CommandOrControl+Shift+I", () => {
+    remote.BrowserWindow.getFocusedWindow().webContents.openDevTools();
+});
+
+window.addEventListener("beforeunload", () => {
+    remote.globalShortcut.unregisterAll();
+});
